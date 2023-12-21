1b2f07366ce9b9428bd492f5a4678b3263e988f0
leo <lseoshnik@mail.ru>
2023-12-21 22:48:06 +0200

    add solution remove-from-list and fix queue.js

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,5 @@
 const { NotImplementedError } = require("../extensions/index.js");
-
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -23,13 +22,19 @@ const { NotImplementedError } = require("../extensions/index.js");
  * }
  */
 function removeKFromList(l, k) {
-  let temp = Array.from(l);
-  for (let elem in temp) {
-    if (temp[elem] === k) {
-      l.splice(elem, 1);
+  const temp = new ListNode(0);
+  temp.next = l;
+
+  let current = temp;
+
+  while (current.next !== null) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
     }
   }
-  return l;
+  return temp.next;
 }
 
 module.exports = {
